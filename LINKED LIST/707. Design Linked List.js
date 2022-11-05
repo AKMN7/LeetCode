caa3b6595b06d2e494ae3c2b5e9c52e39bf056b5
Anas864 <58662561+Anas864@users.noreply.github.com>
2022-11-05 20:00:10 +0300

    Question 707

--- LINKED LIST/707. Design Linked List.js
+++ LINKED LIST/707. Design Linked List.js
@@ -11,31 +11,87 @@ class MyLinkedList {
     }
 
     get(index) {
-        if (index < 0 || index > this.getLength()) return -1;
+        if (index < 0 || index >= this.getLength()) return -1;
 
         let i = 0;
         let node = this.head;
         while (node) {
             if (i == index) return node.val;
             node = node.next;
+            i++;
         }
     }
 
-    // addAtHead() {
+    addAtHead(val) {
+        let newNode = new ListNode(val, this.head);
+        this.head = newNode;
+    }
 
-    // }
+    addAtTail(val) {
 
-    // addAtTail() {
+        if (this.head == null) {
+            this.addAtHead(val);
+            return
+        }
 
-    // }
+        let node = this.head;
+        while (node) {
+            if (node.next == null) {
+                node.next = new ListNode(val);
+                break;
+            }
+            node = node.next
+        }
+    }
 
-    // addAtIndex() {
+    addAtIndex(index, val) {
+        if (index == 0) {
+            this.addAtHead(val);
+            return;
+        }
 
-    // }
+        if (index == this.getLength()) {
+            this.addAtTail(val);
+            return
+        }
 
-    // deleteAtIndex() {
+        if (index > this.getLength()) return;
 
-    // }
+        let node = this.head;
+        let prevNode = node;
+
+        let i = 0;
+        while (node) {
+            if (i == index) {
+                let temp = node;
+                prevNode.next = new ListNode(val, temp);
+                break;
+            }
+
+            prevNode = node;
+            node = node.next;
+            i++;
+        }
+    }
+
+    deleteAtIndex(index) {
+        if (index < 0 || index >= this.getLength()) return;
+        if (index == 0) {
+            this.head = this.head.next;
+            return;
+        }
+
+        let node = this.head;
+        let i = 0;
+        while (node) {
+            if (i == index - 1) {
+                node.next = node.next.next
+                break
+            }
+            node = node.next;
+            i++;
+        }
+    }
 
     getLength() {
         let len = 0;
@@ -48,4 +104,25 @@ class MyLinkedList {
 
         return len;
     }
+
+    printList() {
+        let res = ""
+        let node = this.head;
+        while (node) {
+            res = res + node.val + "->";
+            node = node.next
+        }
+        return res;
+    }
 }
+
+
+let myLinkedtList = new MyLinkedList();
+myLinkedtList.addAtHead(1);
+myLinkedtList.addAtTail(3);
+myLinkedtList.addAtIndex(1, 2);
+console.log(myLinkedtList.printList());
+console.log(myLinkedtList.get(1));
+myLinkedtList.deleteAtIndex(1);
+console.log(myLinkedtList.printList());
+console.log(myLinkedtList.get(1));
