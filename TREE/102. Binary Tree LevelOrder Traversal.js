691673bcfa484cebc06af73405c6aef23d1196d1
Anas864 <58662561+Anas864@users.noreply.github.com>
2022-11-21 04:28:17 +0300

    Minor Edits

--- TREE/102. Binary Tree LevelOrder Traversal.js
+++ TREE/102. Binary Tree LevelOrder Traversal.js
@@ -1,20 +1,20 @@
 function levelOrder(root) {
-    let q = [root];
-    let ans = [];
+	let q = [root];
+	let ans = [];
 
-    while (q[0]) {
-        let row = [];
-        let qlen = q.length;
+	while (q[0]) {
+		let row = [];
+		let qlen = q.length;
 
-        for (let i = 0; i < qlen; i++) {
-            let current = q.shift();
-            row.push(current.val);
-            if (current.left) q.push(current.left);
-            if (current.right) q.push(current.right);
-        }
+		for (let i = 0; i < qlen; i++) {
+			let current = q.shift();
+			row.push(current.val);
+			if (current.left) q.push(current.left);
+			if (current.right) q.push(current.right);
+		}
 
-        ans.push(row);
-    }
+		ans.push(row);
+	}
 
-    return ans;
+	return ans;
 }
