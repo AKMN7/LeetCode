c0f75c42ab28de61bc59241305699c2a5f8d0569
Anas864 <58662561+Anas864@users.noreply.github.com>
2023-02-06 19:28:36 +0300

    Q242

--- HASH TABLE/217. Contains Duplicate.js
+++ HASH TABLE/217. Contains Duplicate.js
@@ -4,11 +4,8 @@
 function containsDuplicate(nums) {
 	let obj = {};
 	for (let i = 0; i < nums.length; i++) {
-		if (obj[nums[i]]) {
-			return true;
-		} else {
-			obj[nums[i]] = true;
-		}
+		if (obj[nums[i]]) return true;
+		obj[nums[i]] = true;
 	}
 	return false;
 }
--- HASH TABLE/242. Valid Anagram.js
+++ HASH TABLE/242. Valid Anagram.js
@@ -1,19 +1,44 @@
+//! Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+//! An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+//! typically using all the original letters exactly once.
+
 function isAnagram(s, t) {
-    if (s.length !== t.length) return false;
+	if (s.length !== t.length) return false;
 
-    let map = {};
+	let map = {};
 
-    for (let i = 0; i < s.length; i++) {
-        map[s[i]] = map[s[i]] ? map[s[i]] + 1 : 1;
-    }
+	for (let i = 0; i < s.length; i++) {
+		map[s[i]] = map[s[i]] ? map[s[i]] + 1 : 1;
+	}
 
-    for (let i = 0; i < t.length; i++) {
-        if (!map[t[i]]) {
-            return false;
-        } else {
-            map[t[i]] = map[t[i]] - 1;
-        }
-    }
+	for (let j = 0; j < t.length; j++) {
+		if (!map[t[j]]) return false;
+		map[t[j]] = map[t[j]] - 1;
+	}
 
-    return true;
+	return true;
 }
+
+console.log(isAnagram("anagram", "nagaram"));
+console.log(isAnagram("rat", "car"));
+
+// function isAnagram(s, t) {
+//     if (s.length !== t.length) return false;
+
+//     let map = {};
+
+//     for (let i = 0; i < s.length; i++) {
+//         map[s[i]] = map[s[i]] ? map[s[i]] + 1 : 1;
+//     }
+
+//     for (let i = 0; i < t.length; i++) {
+//         if (!map[t[i]]) {
+//             return false;
+//         } else {
+//             map[t[i]] = map[t[i]] - 1;
+//         }
+//     }
+
+//     return true;
+// }
