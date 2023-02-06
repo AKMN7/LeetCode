61182aa5579b74686f9d5847c0e28a681d73ca69
Anas864 <58662561+Anas864@users.noreply.github.com>
2023-02-06 19:39:34 +0300

    Q242

--- HASH TABLE/242. Valid Anagram.js
+++ HASH TABLE/242. Valid Anagram.js
@@ -20,25 +20,5 @@ function isAnagram(s, t) {
 	return true;
 }
 
-console.log(isAnagram("anagram", "nagaram"));
-console.log(isAnagram("rat", "car"));
-
-// function isAnagram(s, t) {
-//     if (s.length !== t.length) return false;
-
-//     let map = {};
-
-//     for (let i = 0; i < s.length; i++) {
-//         map[s[i]] = map[s[i]] ? map[s[i]] + 1 : 1;
-//     }
-
-//     for (let i = 0; i < t.length; i++) {
-//         if (!map[t[i]]) {
-//             return false;
-//         } else {
-//             map[t[i]] = map[t[i]] - 1;
-//         }
-//     }
-
-//     return true;
-// }
+console.log(isAnagram("anagram", "nagaram")); //true
+console.log(isAnagram("rat", "car")); //false
