b0c17ad23784e90278ca9e0abe3b6214379c600d
Anas864 <58662561+Anas864@users.noreply.github.com>
2023-02-06 19:11:59 +0300

    Q.217

---
+++ HASH TABLE/217. Contains Duplicate.js
@@ -0,0 +1,14 @@
+//! Given an integer array nums, return true if any value appears at least twice in the array,
+//! and return false if every element is distinct.
+
+function containsDuplicate(nums) {
+	let obj = {};
+	for (let i = 0; i < nums.length; i++) {
+		if (obj[nums[i]]) {
+			return true;
+		} else {
+			obj[nums[i]] = true;
+		}
+	}
+	return false;
+}
