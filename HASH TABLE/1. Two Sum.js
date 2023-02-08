8df2d6b53bd2f80b70497463cf4ba493a8baba7c
Anas864 <58662561+Anas864@users.noreply.github.com>
2023-02-09 00:59:24 +0300

    Refactoring Question One Tow Decrease Total Time Complexity

--- HASH TABLE/1. Two Sum.js
+++ HASH TABLE/1. Two Sum.js
@@ -1,18 +1,36 @@
-function twoSum(nums, target) {
-    let results = [];
+//! Time Complexity O(n^2)
+function twoSumBad(nums, target) {
+	let results = [];
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            if (j != i && nums[i] + nums[j] == target) {
-                let answer = [j, i];
-                if (!results.some(subarray => subarray.every((elm, ind) => elm == answer[ind]))) {
-                    results.push([i, j])
-                }
-            }
-        }
-    }
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = 0; j < nums.length; j++) {
+			if (j != i && nums[i] + nums[j] == target) {
+				let answer = [j, i];
+				if (!results.some((subarray) => subarray.every((elm, ind) => elm == answer[ind]))) {
+					results.push([i, j]);
+				}
+			}
+		}
+	}
 
-    return results;
+	return results;
+}
+
+//! Time Complexity O(n)
+function twoSum(nums, target, map = new Map()) {
+	for (let index = 0; index < nums.length; index++) {
+		const num = nums[index];
+		const complement = target - num;
+
+		const sumIndex = map.get(complement);
+		const isTarget = map.has(complement);
+
+		if (isTarget) return [sumIndex, index];
+
+		map.set(num, index);
+	}
+
+	return [-1, -1];
 }
 
 console.log(twoSum([2, 7, 11, 15], 9));
