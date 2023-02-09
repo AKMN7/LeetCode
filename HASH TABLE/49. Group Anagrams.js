486fa263dda0c0a1c9b6cd09a0bd456910305749
Anas864 <58662561+Anas864@users.noreply.github.com>
2023-02-10 02:19:45 +0300

    Updated Question 42 solution for bettwer time complexity

--- HASH TABLE/49. Group Anagrams.js
+++ HASH TABLE/49. Group Anagrams.js
@@ -1,9 +1,10 @@
-function groupAnagrams(strs) {
-    let map = {};
-    for (let str of strs) {
-        let key = str.split('').sort().join('');
-        if (!map[key]) map[key] = [];
-        map[key].push(str);
-    }
-    return Object.values(map);
+function groupAnagrams(strs, map = new Map()) {
+	for (let i = 0; i < strs.length; i++) {
+		const str = strs[i].split("").sort().join("");
+		if (!map.has(str)) map.set(str, []);
+		map.set(str, [...map.get(str), strs[i]]);
+	}
+	return Array.from(map.values());
 }
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
